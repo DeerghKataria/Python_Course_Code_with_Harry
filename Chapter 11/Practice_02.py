2a0d2b5e25fe5719156d8dbc634529cc55d168d3
DeerghKataria <deerghkataria@gmail.com>
2024-06-30 04:06:12 +0530

    Inheritance practice questions

---
+++ Chapter 11/Practice_02.py
@@ -0,0 +1,13 @@
+class Animals:
+    animalType = "Mammal"
+
+class Pets:
+    petColor = "White"
+
+class Dog:
+    @staticmethod
+    def bark():
+        print("Bark! Bark!")
+
+d = Dog()
+d.bark()
